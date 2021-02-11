c86b91a019a764f04736e806958e0b58a43317f2
ChrisFennek <christian.gebhardt@online.de>
2021-02-11 17:47:23 +0100

    Listening to database

--- app.js
+++ app.js
@@ -1,41 +1,71 @@
-    const messagesList = document.querySelector("messages"); // <ul> with all the <li> messages
-    const messageForm = document.querySelector("#message-form"); // Input form
-    const inputField = document.querySelector("#msg-input");
-    const sendButton = document.querySelector("#msg-btn");
-    const db = firebase.firestore();
-    const chats = db.collection("chats"); // Reference to the chats folder on firebase
+const messagesList = document.querySelector("#messages"); // <ul> with all the <li> messages
+const messageForm = document.querySelector("#message-form"); // Input form
+const inputField = document.querySelector("#msg-input");
+const sendButton = document.querySelector("#msg-btn");
+const db = firebase.firestore();
+const chats = db.collection("chats"); // Reference to the chats folder on firebase
 
-    /*     var test = localStorage.getItem("username");
-        console.log(test); */
-
-
-    // Check for username, if not enter and save to local storage for future
-    document.addEventListener("DOMContentLoaded", () => {
-      if (!localStorage.getItem("username")) {
-        const username = prompt("Username eingeben")
-        localStorage.setItem("username", username)
-      }
-    });
-
-    // Listen to the Senden button
-    messageForm.addEventListener('submit', sendMessage);
-    // Create object with name and messagetext, before check if text is empty
-    function sendMessage(e) {
-      e.preventDefault();
-      const messageText = inputField.value;
-
-      // If message not empty send object to the firebase
-      if (messageText.trim()) {
-        const username = localStorage.getItem("username")
-        const message = {
-          username: username,
-          messageText: messageText
-        }
-        chats.add(message);
-        console.log(message)
-      };
-      // Clear form after sending
-      messageForm.reset();
+/*     var test = localStorage.getItem("username");
+    console.log(test); */
 
 
+// Check for username, if not enter and save to local storage for future
+document.addEventListener("DOMContentLoaded", () => {
+  if (!localStorage.getItem("username")) {
+    const username = prompt("Username eingeben")
+    localStorage.setItem("username", username)
+  }
+});
+
+// Getting messages from the database
+function addChatOnScreen(chat) {
+  const username = localStorage.getItem("username")
+  /* let time = chat.created.toDate();
+   */
+
+  // Messages load, check if its local user
+  let html = `
+        <li class="${chat.username === username ? "msg to-right": "msg"}"><span class = "msg-span">
+        <i class = "name">${chat.username}: </i>${chat.messageText}
+        </span>
+        </li>
+      `
+  // Adding the generated list item message to the chat
+  messagesList.innerHTML += html;
+
+  // Scroll to bottom
+  let window = document.querySelector("#messages-window");
+  window.scrollTop = window.scrollHeight - window.clientHeight;
+}
+
+// Look for added or removed documents in database
+chats.onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
+    const doc = change.doc;
+    console.log(doc)
+    if (change.type === "added") {
+      addChatOnScreen(doc.data());
     }
+  });
+});
+
+
+// Listen to the Senden button
+messageForm.addEventListener('submit', sendMessage);
+// Create object with name and messagetext, before check if text is empty
+function sendMessage(e) {
+  e.preventDefault();
+  const messageText = inputField.value;
+
+  // If message not empty send object to the firebase
+  if (messageText.trim()) {
+    const username = localStorage.getItem("username")
+    const message = {
+      username: username,
+      messageText: messageText
+    }
+    chats.add(message);
+  };
+  // Clear form after sending
+  messageForm.reset();
+}
