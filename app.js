508534a9843ea608aee7c1de8b014822e5ee0fd8
ChrisFennek <christian.gebhardt@online.de>
2021-02-15 18:48:00 +0100

    SignIn

--- app.js
+++ app.js
@@ -6,9 +6,48 @@ const signedIn = document.querySelector("#signed-in");
 const signedOut = document.querySelector("#signed-out");
 const signInBtn = document.querySelector("#sign-in-btn");
 
-const auth = firebase.auth();
 const db = firebase.firestore();
 const chats = db.collection("chats"); // Reference to the chats folder on firebase
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [{
+    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    requireDisplayName: false
+  }]
+});
+
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function () {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: 'loggedIn.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+
+  ],
+
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
 
 // Check for username, if not enter and save to local storage for future
 document.addEventListener("DOMContentLoaded", () => {
